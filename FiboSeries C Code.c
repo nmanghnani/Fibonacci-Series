2bd8088c3520e6f0d5e36330752a5db184e05433
Neha Manghnani <49524335+nmanghnani@users.noreply.github.com>
2019-04-30 10:51:54 -0500

    Update FiboSeries C Code.c

--- FiboSeries C Code.c
+++ FiboSeries C Code.c
@@ -4,7 +4,7 @@ int main()
 {
   int n, first = 0, second = 1, next, F;
  
-  printf("Enter the number of terms\n");
+  printf("Enter number of terms\n");
   scanf("%d", &n);
  
   printf("First %d terms of Fibonacci series are:\n", n);
